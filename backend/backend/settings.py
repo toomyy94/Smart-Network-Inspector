ceab2c058074e584652904e277b071586fd7e611
dani.fsec@gmail.com <daniel.asd.09>
2016-12-20 18:08:40 +0000

    Initial backend project. Added gitignore.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework_swagger',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^', include('core.urls', namespace='backend')),
 ]
