2fdf316a192e4ff1cf30f6307d36aab57c09a20c
toomyy94 <tomasrodrigues@ua.pt>
2016-12-26 17:43:10 +0000

    new icons, tests activity done

--- NetworkInspector/app/src/main/java/tomasr/ua/pt/networkinspector/TestsActivity.java
+++ NetworkInspector/app/src/main/java/tomasr/ua/pt/networkinspector/TestsActivity.java
@@ -1,17 +1,268 @@
 package tomasr.ua.pt.networkinspector;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TestsActivity extends AppCompatActivity {
 
+    final Context context = this;
 
+    //For Download
+    ProgressBar pb;
+    Dialog dialog;
+    int downloadedSize = 0;
+    int totalSize = 0;
+    String dwnload_file_path = "http://coderzheaven.com/sample_folder/sample_file.png";
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tests);
+        //Progress Bar
+        ProgressBar progress_bar = (ProgressBar) findViewById(R.id.progress_bar);
+        progress_bar.setVisibility(View.VISIBLE);
+
+        //TESTS
+        Button test_all = (Button) findViewById(R.id.test_all);
+        test_all.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //ProgressBar
+                ProgressBar progress_bar = (ProgressBar) findViewById(R.id.progress_bar);
+                progress_bar.setVisibility(View.VISIBLE);
+
+                //Test PING
+                String tmp;
+                tmp = ping("www.google.pt");
+                Log.i("ping return", "" + tmp);
+                //Check Test
+                CheckBox ic_test = (CheckBox) findViewById(R.id.ictest);
+                ic_test.setText("Ok!");
+                ic_test.setChecked(true);
+                SystemClock.sleep(1000);
+
+                //Dowload Test
+                new Thread(new Runnable() {
+                    public void run() {
+                        downloadFile();
+                    }
+                }).start();
+                //Check Test
+                SystemClock.sleep(500);
+                CheckBox dd_test = (CheckBox) findViewById(R.id.ddtest);
+                ic_test.setText("Ok!");
+                ic_test.setChecked(true);
+                SystemClock.sleep(1000);
+
+                //Upload Test
+                CheckBox ud_test = (CheckBox) findViewById(R.id.udtest);
+                if (ic_test.isChecked()) {
+                    ic_test.setText("Ok!");
+                    ic_test.setChecked(true);
+                    SystemClock.sleep(1000);
+                }
+
+                SystemClock.sleep(1500);
+                //Test Voice
+                testVoice();
+                //Check Test
+                CheckBox voice_test = (CheckBox) findViewById(R.id.voicetest);
+                voice_test.setText("Ok!");
+                voice_test.setChecked(true);
+                progress_bar.setVisibility(View.INVISIBLE);
+
+            }
+        });
+        progress_bar.setVisibility(View.INVISIBLE);
 
     }
 
+    void downloadFile(){
+
+        try {
+            URL url = new URL(dwnload_file_path);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoOutput(true);
+
+            //connect
+            urlConnection.connect();
+
+            //set the path where we want to save the file
+            File SDCardRoot = Environment.getExternalStorageDirectory();
+            //create a new file, to save the downloaded file
+            File file = new File(SDCardRoot,"downloaded_file.png");
+
+            FileOutputStream fileOutput = new FileOutputStream(file);
+
+            //Stream used for reading the data from the internet
+            InputStream inputStream = urlConnection.getInputStream();
+
+            //this is the total size of the file which we are downloading
+            totalSize = urlConnection.getContentLength();
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    pb.setMax(totalSize);
+                }
+            });
+
+            //create a buffer...
+            byte[] buffer = new byte[1024];
+            int bufferLength = 0;
+
+            while ( (bufferLength = inputStream.read(buffer)) > 0 ) {
+                fileOutput.write(buffer, 0, bufferLength);
+                downloadedSize += bufferLength;
+                // update the progressbar //
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        pb.setProgress(downloadedSize);
+                        float per = ((float)downloadedSize/totalSize) * 100;
+                    }
+                });
+            }
+            //close the output stream when complete //
+            fileOutput.close();
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    // pb.dismiss(); // if you want close it..
+                }
+            });
+
+        } catch (final MalformedURLException e) {
+            showError("Error : MalformedURLException " + e);
+            e.printStackTrace();
+        } catch (final IOException e) {
+            showError("Error : IOException " + e);
+            e.printStackTrace();
+        }
+        catch (final Exception e) {
+            showError("Error : Please check your internet connection " + e);
+        }
+    }
+
+    void showError(final String err){
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(TestsActivity.this, err, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public String ping(String url) {
+        String str = "";
+        try {
+            Process process = Runtime.getRuntime().exec(
+                    "/system/bin/ping -c 8 " + url);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    process.getInputStream()));
+            int i;
+            char[] buffer = new char[4096];
+            StringBuffer output = new StringBuffer();
+            while ((i = reader.read(buffer)) > 0)
+                output.append(buffer, 0, i);
+            reader.close();
+
+            // body.append(output.toString()+"\n");
+            str = output.toString();
+            // Log.d(TAG, str);
+        } catch (IOException e) {
+            // body.append("Error\n");
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    public void testVoice(){
+        // add PhoneStateListener
+        PhoneCallListener phoneListener = new PhoneCallListener();
+        TelephonyManager telephonyManager = (TelephonyManager) this
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:123"));
+        startActivity(callIntent);
+    }
+
+
+    //monitor phone call activities
+    private class PhoneCallListener extends PhoneStateListener {
+
+        private boolean isPhoneCalling = false;
+
+        String LOG_TAG = "LOGGING 123";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            if (TelephonyManager.CALL_STATE_RINGING == state) {
+                // phone ringing
+                Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+            }
+
+            if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+                // active
+                Log.i(LOG_TAG, "OFFHOOK");
+
+                isPhoneCalling = true;
+            }
+
+            if (TelephonyManager.CALL_STATE_IDLE == state) {
+                // run when class initial and phone call ended,
+                // need detect flag from CALL_STATE_OFFHOOK
+                Log.i(LOG_TAG, "IDLE");
+
+                if (isPhoneCalling) {
+
+                    Log.i(LOG_TAG, "restart app");
+
+                    // restart app
+//                    Intent i = getBaseContext().getPackageManager()
+//                            .getLaunchIntentForPackage(
+//                                    getBaseContext().getPackageName());
+//                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(i);
+
+                    isPhoneCalling = false;
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
 }
+
+
